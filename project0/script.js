ed2fa40a0c0f59bd3394e1ab495752ae849ae508
Pierluca Del Buono <pierluca@amazon.com>
2020-10-22 07:34:17 +0200

    adding Project 0 base

---
+++ project0/script.js
@@ -0,0 +1,15 @@
+const classNames = {
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+}
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+function newTodo() {
+  let itemCount = document.getElementById('item-count')
+  itemCount.innerHTML = Number(itemCount.innerHTML) + gi1
+}
