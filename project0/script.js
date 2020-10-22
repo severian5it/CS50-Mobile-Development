92ed3cff35274129e23945ae26e63e6420a6411b
Pierluca Del Buono <pierluca@amazon.com>
2020-10-22 07:53:37 +0200

    adding todo when clicked by JS

--- project0/script.js
+++ project0/script.js
@@ -11,5 +11,14 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
   let itemCount = document.getElementById('item-count')
+
   itemCount.innerHTML = Number(itemCount.innerHTML) + 1
+
+  document.getElementById('todo-list').innerHTML += `<li>  
+                                                            <div class="todo-container">
+                                                            .. 
+                                                            <input type="checkbox" class = "todo-checkbox" id="toIncrement1" name="check1" value="increment">
+                                                            </div>
+                                                      </li>`;
+
 }
