7fc42b69bdd89af1391002efac45aa42e22afa63
Pierluca Del Buono <pierluca@amazon.com>
2020-10-24 16:58:23 +0200

    toggling checked attribute on react todo

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -6,7 +6,7 @@ let id = 0
 
 const Todo = props => (
   <li>
-    <input type="checkbox" />
+    <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle} />
     <button onClick={props.onDelete}>delete</button>
     <span>{props.todo.text}</span>
   </li>
@@ -23,13 +23,27 @@ class App extends React.Component {
   addTodo() {
     const text = prompt("insert a Todo")
     this.setState({
-      todos:[...this.state.todos,{id: id++, text: text}]
+      todos:[...this.state.todos,{id: id++, text: text, checked: false}]
     })
   }
 
   removeTodo(id) {
     this.setState({
       todos: this.state.todos.filter( todo => todo.id != id)
+    })
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map( todo => {
+        if (todo.id != id) return todo
+        return {
+            id: todo.id,
+            text: todo.text,
+            checked: !todo.checked,
+        }
+      
+      })
     })
   }
 
@@ -43,6 +57,7 @@ class App extends React.Component {
           {this.state.todos.map(
             todo => (
             <Todo 
+            onToggle={() => this.toggleTodo(todo.id)}
             onDelete={() => this.removeTodo(todo.id)}
             todo={todo}>
             </Todo>)
