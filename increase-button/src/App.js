61ffe426786845d74f1d12b0943b1cc6f861d75e
Pierluca Del Buono <pierluca@amazon.com>
2020-10-24 15:03:23 +0200

    rewriting app.js to increase button

--- increase-button/src/App.js
+++ increase-button/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+    }
+  }
+  increaseCount(){
+    this.setState({count: this.state.count + 1})
+  }
+  render() {
+    return(
+      <div style={styles}>
+        <button onClick={() => this.increaseCount()}> Increase </button>
+        <h2>{this.state.count}</h2>
+      </div>
+      ) 
+    };
+};
 
 export default App;
